0bcc498b197155ae53c169f5640d1809a7036c99
André <AndreR@ppedv.de>
2022-06-23 13:41:11 +0200

    Jetzt in Hübsch

--- HalloEfCore/HalloEfCore/MainWindow.xaml.cs
+++ HalloEfCore/HalloEfCore/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ namespace HalloEfCore
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow 
     {
         public MainWindow()
         {
